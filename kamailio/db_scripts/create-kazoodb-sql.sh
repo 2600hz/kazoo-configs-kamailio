45d4aa17e301888ad891e6ef2de64ae5b960241a
Sergey Safarov <s.safarov@gmail.com>
2018-01-27 18:31:20 +0000

    db_scripts: Addeded creation of active_watchers_log table for PostgreSQL. db_scripts: Adjusted location_attrs table to store long avalue. db_scripts: KAMAILIO_SHARE_DIR and RESULTED_SQL is made configurable via enviroment vars db_scripts: DB scpectific function separated to dedicated files

--- kamailio/db_scripts/create-kazoodb-sql.sh
+++ kamailio/db_scripts/create-kazoodb-sql.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -e
 
-KAMAILIO_SHARE_DIR=/usr/share/kamailio
+KAMAILIO_SHARE_DIR=${KAMAILIO_SHARE_DIR:-/usr/share/kamailio}
 DB_ENGINE=${DB_ENGINE:-postgres}
-RESULTED_SQL=/tmp/kamailio_initdb.sql
+RESULTED_SQL=${RESULTED_SQL:-/tmp/kamailio_initdb.sql}
 
-get_sql_filelist() {
+. ./$DB_ENGINE-spectific --source-only
+
+sql_filelist() {
 cat << EOF
 acc-create.sql
 lcr-create.sql
@@ -48,33 +50,13 @@ uid_uri_db-create.sql
 EOF
 }
 
-resulted_sql_header() {
-cat << EOF
-BEGIN TRANSACTION;
-CREATE TABLE version (
-    table_name VARCHAR(32) NOT NULL,
-    table_version INTEGER DEFAULT 0 NOT NULL,
-    CONSTRAINT version_table_name_idx UNIQUE (table_name)
-);
-INSERT INTO version VALUES('version',1);
-CREATE TABLE event_list ( event varchar(25) PRIMARY KEY NOT NULL);
-INSERT INTO event_list VALUES('dialog');
-INSERT INTO event_list VALUES('presence');
-INSERT INTO event_list VALUES('message-summary');
-EOF
-}
-
-resulted_sql_footer() {
-cat << EOF
-COMMIT;
-EOF
-}
-
 echo "Creating kamailio database init file in '$RESULTED_SQL'"
 
-resulted_sql_header > $RESULTED_SQL
-for i in $(get_sql_filelist); do
+sql_db_pre_setup > $RESULTED_SQL
+sql_header > $RESULTED_SQL
+for i in $(sql_filelist); do
     cat $KAMAILIO_SHARE_DIR/$DB_ENGINE/$i >> $RESULTED_SQL
 done
-resulted_sql_footer >> $RESULTED_SQL
+sql_extra_tables >> $RESULTED_SQL
+sql_footer >> $RESULTED_SQL
 exit 0
