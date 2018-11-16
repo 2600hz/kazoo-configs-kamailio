70e7aef55833555fe66707da8ebd04424271a5c7
lazedo <luis.azedo@factorlusitano.com>
2018-11-16 19:08:32 +0000

    allow db downgrade

--- kamailio/db_scripts/check-kazoodb-sql.sh
+++ kamailio/db_scripts/check-kazoodb-sql.sh
@@ -15,7 +15,7 @@ DB_CURRENT_DB=${DB_LOCATION:-/etc/kazoo/kamailio}/kazoo.db
 DB_CURRENT_VERSION=`KazooDB -db ${DB_CURRENT_DB} "select sum(table_version) from version;"`
 
 
-if [[ $DB_CURRENT_VERSION -lt $DB_VERSION ]]; then
+if [[ $DB_CURRENT_VERSION -ne $DB_VERSION ]]; then
    echo "db required version is ${DB_VERSION}, existing version is ${DB_CURRENT_VERSION}, applying diff"
    KazooDB-diff --schema  ${DB_CURRENT_DB} ${TEMP_DB} | KazooDB -db ${DB_CURRENT_DB}
    KazooDB-diff --table version ${DB_CURRENT_DB} ${TEMP_DB} | KazooDB -db ${DB_CURRENT_DB}
