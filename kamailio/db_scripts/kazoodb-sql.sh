2997c5191ce1bd8b01d1c8736e16e074de87871f
lazedo <luis.azedo@factorlusitano.com>
2019-03-02 00:15:25 +0000

    include all sql files from shared dir

--- kamailio/db_scripts/kazoodb-sql.sh
+++ kamailio/db_scripts/kazoodb-sql.sh
@@ -7,47 +7,7 @@ RESULTED_SQL=${RESULTED_SQL:-/tmp/kamailio_initdb.sql}
 . $(dirname $0)/$DB_ENGINE-specific --source-only
 
 sql_filelist() {
-cat << EOF
-acc-create.sql
-lcr-create.sql
-domain-create.sql
-group-create.sql
-permissions-create.sql
-registrar-create.sql
-usrloc-create.sql
-msilo-create.sql
-alias_db-create.sql
-uri_db-create.sql
-speeddial-create.sql
-avpops-create.sql
-auth_db-create.sql
-pdt-create.sql
-dialog-create.sql
-dispatcher-create.sql
-dialplan-create.sql
-topos-create.sql
-presence-create.sql
-rls-create.sql
-imc-create.sql
-cpl-create.sql
-siptrace-create.sql
-domainpolicy-create.sql
-carrierroute-create.sql
-userblacklist-create.sql
-htable-create.sql
-purple-create.sql
-uac-create.sql
-pipelimit-create.sql
-mtree-create.sql
-sca-create.sql
-mohqueue-create.sql
-rtpproxy-create.sql
-uid_auth_db-create.sql
-uid_avp_db-create.sql
-uid_domain-create.sql
-uid_gflags-create.sql
-uid_uri_db-create.sql
-EOF
+  echo `ls -A1 ${KAMAILIO_SHARE_DIR}/${DB_ENGINE}/*.sql | grep -v standard | tr '\n' '\0' | xargs -0 -n 1 basename | sort`
 }
 
 sql_all_header() {
